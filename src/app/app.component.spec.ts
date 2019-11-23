100a4d05e97ffd767602721dc38094e087fe8449
Rinold <clickboot.it@gmail.com>
2019-11-23 19:49:46 +0530

    Setting up basic angular 8 environment.

---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,31 @@
+import {async, TestBed} from '@angular/core/testing';
+import {AppComponent} from './app.component';
+
+describe('AppComponent', () => {
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        AppComponent
+      ],
+    }).compileComponents();
+  }));
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it(`should have as title 'course-status-scatter-chart'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app.title).toEqual('course-status-scatter-chart');
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('.content span').textContent).toContain('course-status-scatter-chart app is running!');
+  });
+});
